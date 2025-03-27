32ab61d0dff33554c453ceb743a28752d2462d13
Miguel Carbajal <miguelcarbajal@outlook.com>
2025-03-26 20:40:16 -0500

    feat: create contact dto documentation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+
+  const config = new DocumentBuilder().setTitle('Contacts API').setDescription('Contacts API endpoints documentation').setVersion('1.0').addTag('contacts').build()
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('api', app, document)
+
   await app.listen(3000)
 }
 bootstrap()
--- src/modules/core/contact/dto/create-contact.dto.ts
+++ src/modules/core/contact/dto/create-contact.dto.ts
@@ -5,46 +5,56 @@ import { CreatePhoneNumberDto } from '../../phone-number/dto/create-phone-number
 import { CreateEmailDto } from '../../email/dto/create-email.dto'
 import { CreateCompanyDto } from '../../company/dto/create-company.dto'
 import { CreateGroupDto } from '../../group/dto/create-group.dto'
+import { ApiPropertyOptional } from '@nestjs/swagger'
 
 export class CreateContactDto {
+  @ApiPropertyOptional({ description: 'Person information, required when companyId and company are not provided', required: false, type: CreatePersonDto })
   @IsOptional()
   @ValidateNested()
   @Type(() => CreatePersonDto)
   person?: CreatePersonDto
 
+  @ApiPropertyOptional({ description: 'Company information, required when person and companyId are not provided', required: false })
   @IsOptional()
   @ValidateNested()
   @Type(() => CreateCompanyDto)
   company?: CreateCompanyDto
 
+  @ApiPropertyOptional({ description: 'Existing company identifier, required when person and company are not provided', required: false })
   @IsOptional()
   @IsUUID()
   companyId?: string
 
+  @ApiPropertyOptional({ required: false, isArray: true, minItems: 1, type: [CreatePhoneNumberDto] })
   @IsOptional()
   @IsArray()
   @ValidateNested()
   @Type(() => CreatePhoneNumberDto)
   phoneNumbers?: Array<CreatePhoneNumberDto>
 
+  @ApiPropertyOptional({ required: false, isArray: true, minItems: 1, type: [CreateEmailDto] })
   @IsOptional()
   @IsArray()
   @ValidateNested()
   @Type(() => CreateEmailDto)
   emails?: Array<CreateEmailDto>
 
+  @ApiPropertyOptional({ description: 'Expiration date when contact will be automatically deleted', required: false })
   @IsOptional()
   @IsDateString()
   expiration?: Date
 
+  @ApiPropertyOptional({ required: false })
   @IsOptional()
   @IsString()
   note?: string
 
+  @ApiPropertyOptional({ description: 'Existing group identifier', nullable: true, required: false })
   @IsOptional()
   @IsUUID()
   groupId?: string
 
+  @ApiPropertyOptional({ description: 'Group information', nullable: true, required: false })
   @IsOptional()
   @ValidateNested()
   @Type(() => CreateGroupDto)
